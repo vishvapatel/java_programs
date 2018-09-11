1e4196dcf22f563c197f6c5a4de8a10da1d6a99a
vishvapatel <vnspvt.123@gmail.com>
2018-09-11 16:21:11 -0700

    Added Comments

--- Get_System_Date.java
+++ Get_System_Date.java
@@ -1,7 +1,12 @@
-import Date.*;
+/* This program demonstrates the use of the java package
+* @author vishva patel
+* Editor Intellij Idea
+* Github vishvapatel/java_programs
+*/
+import Date.*;    //importing the userdefined Date Package.
 public class Get_System_Date {
     public static void main(String args[]){
-        System_date sd = new System_date();
-        sd.get_System_Date();
+        System_date sd = new System_date();  //Creating object for the class in the Date package
+        sd.get_System_Date(); //calling the ouptput fuction in System_date class which will display the System_date 
     }
 }
